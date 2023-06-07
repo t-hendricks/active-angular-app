2fcacf091f392c5cf3b5310d9060a133721ab6ff
Trevor H <h.trevs@outlook.com>
2023-06-07 13:36:34 -0500

    Add Post component child route to profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ActivityComponent } from './activity/activity.component';
 import { ProfileComponent } from './profile/profile.component';
+import { PostsComponent } from './profile/posts/posts.component';
 
-const routes: Routes = [ 
+const routes: Routes = [
   {
     path: 'activity',
     component: ActivityComponent
   },
   {
     path: 'profile',
-    component: ProfileComponent
+    component: ProfileComponent,
+    children: [
+      {
+        path: 'posts',
+        component: PostsComponent
+      }
+    ],
   }
 ];
 
