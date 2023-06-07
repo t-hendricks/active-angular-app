b3b7362a6f6c8d63d9fb954382658d4f2d6d62b4
Trevor H <h.trevs@outlook.com>
2023-06-07 10:25:48 -0500

    Add profile component route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ActivityComponent } from './activity/activity.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [ 
   {
     path: 'activity',
     component: ActivityComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
   }
 ];
 
