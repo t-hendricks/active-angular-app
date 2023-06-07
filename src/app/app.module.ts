b65880d9f45dbead9781591d52d92e0b7aa1aaee
Trevor H <h.trevs@outlook.com>
2023-06-07 12:06:16 -0500

    Imported profile module for profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { ProfileModule } from './profile/profile.module';
 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { ActivityComponent } from './activity/activity.component';
-import { ProfileComponent } from './profile/profile.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
     ActivityComponent,
-    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProfileModule
   ],
   providers: [],
   bootstrap: [AppComponent]
