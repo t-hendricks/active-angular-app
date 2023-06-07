29c310a62bfe4331cd20a22ca416a112d9dfc2c1
Trevor H <h.trevs@outlook.com>
2023-06-07 10:03:24 -0500

    Add activity component route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActivityComponent } from './activity/activity.component';
 
-const routes: Routes = [ ];
+const routes: Routes = [ 
+  {
+    path: 'activity',
+    component: ActivityComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
