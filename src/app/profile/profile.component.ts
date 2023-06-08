0e6c53de8d328b88d31ad055bb4d1c368d8d941f
Trevor H <h.trevs@outlook.com>
2023-06-08 13:04:42 -0500

    Add function to get current logged in user content

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  profile: any;
 
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.getCurrentUserContent();
+  }
+
+  getCurrentUserContent() {
+    this.authService.getCurrentUserContent()
+    .subscribe(res => {
+      console.log(res);
+      this.profile = res;
+      this.authService.profile = res;
+    })
+  }
 }
