4c80468fba45482e41a04b237a482620dd054444
Trevor H <h.trevs@outlook.com>
2023-06-08 09:58:58 -0500

    Add a regist function to make a http post request

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  isLoggedIn: boolean;
+  token: string = '';
   
-  constructor() { 
-    this.isLoggedIn = false;
+  constructor(private http: HttpClient) { }
+
+  register(username: string, password: string) {
+    return this.http
+    .post(`http://localhost:8080/auth/users/register`, {userName: username, password: password});
   }
 }
