3b13df0a27b80887e78130bc4141590db7f6225c
Trevor H <h.trevs@outlook.com>
2023-06-08 14:03:26 -0500

    Add subscription onInit to getRandomActivity http function

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivityService } from '../services/activity/activity.service';
 
 @Component({
   selector: 'app-activity',
   templateUrl: './activity.component.html',
   styleUrls: ['./activity.component.css']
 })
-export class ActivityComponent {
+export class ActivityComponent implements OnInit {
+  activity: any;
+
+  constructor(private activityService: ActivityService) {}
+
+  ngOnInit() {
+    this.activityService.getRandomActivity()
+    .subscribe(res => {
+      console.log(res);
+      
+      this.activity = res;
+    })
+  }
 
 }
