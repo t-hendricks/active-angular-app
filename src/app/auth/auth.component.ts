ee0a62d205bd5ceb8599d32594f2a6d10e536faf
Trevor H <h.trevs@outlook.com>
2023-06-08 09:57:55 -0500

    Add register function to subscribe to an http service function

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
+  username: string = '';
+  password: string = '';
+  response: any;
 
+  constructor(private authService: AuthService) { }
+
+  register(username: string, password: string) {
+    this.authService.register(username, password)
+    .subscribe({
+      next: data => {
+        this.response = data;
+      },
+      error: error => {
+        this.response = error;
+      }
+    });
+  }
+
+  login() {
+    console.log(this.response);
+  }
 }
