79025edfa265958a234e77f7fa43cd5731261780
Trevor H <h.trevs@outlook.com>
2023-06-08 22:51:27 -0500

    Add ng on init

--- src/app/profile/posts/posts.component.ts
+++ src/app/profile/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivityService } from 'src/app/services/activity/activity.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
@@ -7,11 +7,23 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
   profile: any;
 
   constructor(private authService: AuthService, private activityService: ActivityService) { 
     this.profile = this.authService.profile;
+  }
+
+  ngOnInit() {
+    this.getCurrentUserContent();
+  }
+
+  getCurrentUserContent() {
+    this.authService.getCurrentUserContent()
+    .subscribe(res => {
+      this.profile = res;
+      this.authService.profile = res;
+    })
   }
 
   addLike(id: number) {
